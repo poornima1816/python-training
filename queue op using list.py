c55a94198cf8933a80d10cc50581fc26f1cb7763
poornima1816 <165008229+poornima1816@users.noreply.github.com>
2024-07-12 06:43:50 +0530

    Add files via upload

---
+++ queue op using list.py
@@ -0,0 +1,13 @@
+queue=[]
+
+queue.append(23)
+queue.append(3)
+queue.append(2)
+queue.append(32)
+queue.append(45)
+queue.append(9)
+queue.append(8)
+print(queue)
+queue.pop(0)
+print(queue)
+print(len(queue))
